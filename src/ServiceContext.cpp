0a332c0253fd70c640244016adb284c4b596b8af
KoynovStas <skojnov@yandex.ru>
2017-03-13 15:57:43 +0300

    add method ServiceContext::getDeviceServiceCapabilities
    method return tds__DeviceServiceCapabilities need for
    impl DeviceBindingService::GetServiceCapabilities

--- src/ServiceContext.cpp
+++ src/ServiceContext.cpp
@@ -21,3 +21,61 @@ ServiceContext::ServiceContext():
 {
 }
 
+
+
+tds__DeviceServiceCapabilities *ServiceContext::getDeviceServiceCapabilities(soap *soap)
+{
+    tds__DeviceServiceCapabilities *capabilities = soap_new_tds__DeviceServiceCapabilities(soap);
+
+    capabilities->Network = soap_new_tds__NetworkCapabilities(soap);
+
+    capabilities->Network->IPFilter            = soap_new_ptr(soap, false);
+    capabilities->Network->ZeroConfiguration   = soap_new_ptr(soap, false);
+    capabilities->Network->IPVersion6          = soap_new_ptr(soap, false);
+    capabilities->Network->DynDNS              = soap_new_ptr(soap, false);
+    capabilities->Network->Dot11Configuration  = soap_new_ptr(soap, false);
+    capabilities->Network->Dot1XConfigurations = soap_new_ptr(soap, 0);
+    capabilities->Network->HostnameFromDHCP    = soap_new_ptr(soap, false);
+    capabilities->Network->NTP                 = soap_new_ptr(soap, 0);
+    capabilities->Network->DHCPv6              = soap_new_ptr(soap, false);
+
+
+    capabilities->Security = soap_new_tds__SecurityCapabilities(soap);
+
+    capabilities->Security->TLS1_x002e0          = soap_new_ptr(soap, false);
+    capabilities->Security->TLS1_x002e1          = soap_new_ptr(soap, false);
+    capabilities->Security->TLS1_x002e2          = soap_new_ptr(soap, false);
+    capabilities->Security->OnboardKeyGeneration = soap_new_ptr(soap, false);
+    capabilities->Security->AccessPolicyConfig   = soap_new_ptr(soap, false);
+    capabilities->Security->DefaultAccessPolicy  = soap_new_ptr(soap, false);
+    capabilities->Security->Dot1X                = soap_new_ptr(soap, false);
+    capabilities->Security->RemoteUserHandling   = soap_new_ptr(soap, false);
+    capabilities->Security->X_x002e509Token      = soap_new_ptr(soap, false);
+    capabilities->Security->SAMLToken            = soap_new_ptr(soap, false);
+    capabilities->Security->KerberosToken        = soap_new_ptr(soap, false);
+    capabilities->Security->UsernameToken        = soap_new_ptr(soap, false);
+    capabilities->Security->HttpDigest           = soap_new_ptr(soap, false);
+    capabilities->Security->RELToken             = soap_new_ptr(soap, false);
+    capabilities->Security->MaxUsers             = soap_new_ptr(soap, 0);
+    capabilities->Security->MaxUserNameLength    = soap_new_ptr(soap, 0);
+    capabilities->Security->MaxPasswordLength    = soap_new_ptr(soap, 0);
+
+
+    capabilities->System = soap_new_tds__SystemCapabilities(soap);
+
+    capabilities->System->DiscoveryResolve       = soap_new_ptr(soap, true);
+    capabilities->System->DiscoveryBye           = soap_new_ptr(soap, true);
+    capabilities->System->RemoteDiscovery        = soap_new_ptr(soap, true);
+    capabilities->System->SystemBackup           = soap_new_ptr(soap, false);
+    capabilities->System->SystemLogging          = soap_new_ptr(soap, false);
+    capabilities->System->FirmwareUpgrade        = soap_new_ptr(soap, false);
+    capabilities->System->HttpFirmwareUpgrade    = soap_new_ptr(soap, false);
+    capabilities->System->HttpSystemBackup       = soap_new_ptr(soap, false);
+    capabilities->System->HttpSystemLogging      = soap_new_ptr(soap, false);
+    capabilities->System->HttpSupportInformation = soap_new_ptr(soap, false);
+    capabilities->System->StorageConfiguration   = soap_new_ptr(soap, false);
+
+
+    return capabilities;
+}
+
--- src/ServiceContext.h
+++ src/ServiceContext.h
@@ -30,7 +30,33 @@ class ServiceContext
         std::string hardware_id;
 
         std::vector<std::string> scope;
+
+
+        // service capabilities
+        tds__DeviceServiceCapabilities* getDeviceServiceCapabilities(struct soap* soap);
+//        trt__Capabilities*  getMediaServiceCapabilities    (struct soap* soap);
+//        timg__Capabilities* getImagingServiceCapabilities  (struct soap* soap);
+//        trc__Capabilities*  getRecordingServiceCapabilities(struct soap* soap);
+//        tse__Capabilities*  getSearchServiceCapabilities   (struct soap* soap);
+//        trv__Capabilities*  getReceiverServiceCapabilities (struct soap* soap);
+//        trp__Capabilities*  getReplayServiceCapabilities   (struct soap* soap);
+//        tev__Capabilities*  getEventServiceCapabilities    (struct soap* soap);
+//        tls__Capabilities*  getDisplayServiceCapabilities  (struct soap* soap);
+//        tmd__Capabilities*  getDeviceIOServiceCapabilities (struct soap* soap);
 };
+
+
+
+
+
+template<typename T>
+T* soap_new_ptr(struct soap* soap, T value)
+{
+    T* ptr = (T*)soap_malloc(soap, sizeof(T));
+    *ptr = value;
+
+    return ptr;
+}
 
 
 

04cb2cad8c6598397508be704f186fb237a6873e
KoynovStas <skojnov@yandex.ru>
2020-10-17 12:20:07 +0400

    PTZ: refactoring code (add space)

--- src/ServicePTZ.cpp
+++ src/ServicePTZ.cpp
@@ -1,7 +1,7 @@
 /*
  --------------------------------------------------------------------------
  ServicePTZ.cpp
- 
+
  Implementation of functions (methods) for the service:
  ONVIF ptz.wsdl server side
 -----------------------------------------------------------------------------
@@ -14,18 +14,23 @@
 
 
 
+
+
 int PTZBindingService::GetServiceCapabilities(_tptz__GetServiceCapabilities *tptz__GetServiceCapabilities, _tptz__GetServiceCapabilitiesResponse &tptz__GetServiceCapabilitiesResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
-
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::GetConfigurations(_tptz__GetConfigurations *tptz__GetConfigurations, _tptz__GetConfigurationsResponse &tptz__GetConfigurationsResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int GetPTZPreset(struct soap *soap, tt__PTZPreset* ptzp, int number)
 {
@@ -42,6 +47,8 @@ int GetPTZPreset(struct soap *soap, tt__PTZPreset* ptzp, int number)
 
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::GetPresets(_tptz__GetPresets *tptz__GetPresets, _tptz__GetPresetsResponse &tptz__GetPresetsResponse)
 {
@@ -60,17 +67,23 @@ int PTZBindingService::GetPresets(_tptz__GetPresets *tptz__GetPresets, _tptz__Ge
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::SetPreset(_tptz__SetPreset *tptz__SetPreset, _tptz__SetPresetResponse &tptz__SetPresetResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::RemovePreset(_tptz__RemovePreset *tptz__RemovePreset, _tptz__RemovePresetResponse &tptz__RemovePresetResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::GotoPreset(_tptz__GotoPreset *tptz__GotoPreset, _tptz__GotoPresetResponse &tptz__GotoPresetResponse)
 {
@@ -98,17 +111,23 @@ int PTZBindingService::GotoPreset(_tptz__GotoPreset *tptz__GotoPreset, _tptz__Go
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetStatus(_tptz__GetStatus *tptz__GetStatus, _tptz__GetStatusResponse &tptz__GetStatusResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetConfiguration(_tptz__GetConfiguration *tptz__GetConfiguration, _tptz__GetConfigurationResponse &tptz__GetConfigurationResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int GetPTZNode(struct soap *soap, tt__PTZNode* ptzn)
 {
@@ -192,6 +211,8 @@ int GetPTZNode(struct soap *soap, tt__PTZNode* ptzn)
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetNodes(_tptz__GetNodes *tptz__GetNodes, _tptz__GetNodesResponse &tptz__GetNodesResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
@@ -205,6 +226,8 @@ int PTZBindingService::GetNodes(_tptz__GetNodes *tptz__GetNodes, _tptz__GetNodes
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetNode(_tptz__GetNode *tptz__GetNode, _tptz__GetNodeResponse &tptz__GetNodeResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
@@ -215,17 +238,23 @@ int PTZBindingService::GetNode(_tptz__GetNode *tptz__GetNode, _tptz__GetNodeResp
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::SetConfiguration(_tptz__SetConfiguration *tptz__SetConfiguration, _tptz__SetConfigurationResponse &tptz__SetConfigurationResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetConfigurationOptions(_tptz__GetConfigurationOptions *tptz__GetConfigurationOptions, _tptz__GetConfigurationOptionsResponse &tptz__GetConfigurationOptionsResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::GotoHomePosition(_tptz__GotoHomePosition *tptz__GotoHomePosition, _tptz__GotoHomePositionResponse &tptz__GotoHomePositionResponse)
 {
@@ -242,11 +271,15 @@ int PTZBindingService::GotoHomePosition(_tptz__GotoHomePosition *tptz__GotoHomeP
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::SetHomePosition(_tptz__SetHomePosition *tptz__SetHomePosition, _tptz__SetHomePositionResponse &tptz__SetHomePositionResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::ContinuousMove(_tptz__ContinuousMove *tptz__ContinuousMove, _tptz__ContinuousMoveResponse &tptz__ContinuousMoveResponse)
 {
@@ -277,6 +310,8 @@ int PTZBindingService::ContinuousMove(_tptz__ContinuousMove *tptz__ContinuousMov
 
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::RelativeMove(_tptz__RelativeMove *tptz__RelativeMove, _tptz__RelativeMoveResponse &tptz__RelativeMoveResponse)
 {
@@ -316,17 +351,23 @@ int PTZBindingService::RelativeMove(_tptz__RelativeMove *tptz__RelativeMove, _tp
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::SendAuxiliaryCommand(_tptz__SendAuxiliaryCommand *tptz__SendAuxiliaryCommand, _tptz__SendAuxiliaryCommandResponse &tptz__SendAuxiliaryCommandResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::AbsoluteMove(_tptz__AbsoluteMove *tptz__AbsoluteMove, _tptz__AbsoluteMoveResponse &tptz__AbsoluteMoveResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::Stop(_tptz__Stop *tptz__Stop, _tptz__StopResponse &tptz__StopResponse)
 {
@@ -339,11 +380,15 @@ int PTZBindingService::Stop(_tptz__Stop *tptz__Stop, _tptz__StopResponse &tptz__
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetPresetTours(_tptz__GetPresetTours *tptz__GetPresetTours, _tptz__GetPresetToursResponse &tptz__GetPresetToursResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::GetPresetTour(_tptz__GetPresetTour *tptz__GetPresetTour, _tptz__GetPresetTourResponse &tptz__GetPresetTourResponse)
 {
@@ -351,11 +396,15 @@ int PTZBindingService::GetPresetTour(_tptz__GetPresetTour *tptz__GetPresetTour, 
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::GetPresetTourOptions(_tptz__GetPresetTourOptions *tptz__GetPresetTourOptions, _tptz__GetPresetTourOptionsResponse &tptz__GetPresetTourOptionsResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::CreatePresetTour(_tptz__CreatePresetTour *tptz__CreatePresetTour, _tptz__CreatePresetTourResponse &tptz__CreatePresetTourResponse)
 {
@@ -363,11 +412,15 @@ int PTZBindingService::CreatePresetTour(_tptz__CreatePresetTour *tptz__CreatePre
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::ModifyPresetTour(_tptz__ModifyPresetTour *tptz__ModifyPresetTour, _tptz__ModifyPresetTourResponse &tptz__ModifyPresetTourResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::OperatePresetTour(_tptz__OperatePresetTour *tptz__OperatePresetTour, _tptz__OperatePresetTourResponse &tptz__OperatePresetTourResponse)
 {
@@ -375,11 +428,15 @@ int PTZBindingService::OperatePresetTour(_tptz__OperatePresetTour *tptz__Operate
     return SOAP_OK;
 }
 
+
+
 int PTZBindingService::RemovePresetTour(_tptz__RemovePresetTour *tptz__RemovePresetTour, _tptz__RemovePresetTourResponse &tptz__RemovePresetTourResponse)
 {
     DEBUG_MSG("PTZ: %s\n", __FUNCTION__);
     return SOAP_OK;
 }
+
+
 
 int PTZBindingService::GetCompatibleConfigurations(_tptz__GetCompatibleConfigurations *tptz__GetCompatibleConfigurations, _tptz__GetCompatibleConfigurationsResponse &tptz__GetCompatibleConfigurationsResponse)
 {
